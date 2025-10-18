ee40fa1061c535f08b1708ee0490e61dd0c0655f
trishamlpz <160499672+alliahf23@users.noreply.github.com>
2025-10-18 12:39:22 +0800

    undo

--- LakbayCalamba/resources/views/home.blade.php
+++ LakbayCalamba/resources/views/home.blade.php
@@ -50,7 +50,7 @@
         </select>
     </div>
     
-    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 mb-8 justify-items-center" id="establishmentsGrid"> 
+    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mb-8" id="establishmentsGrid"> 
         @foreach($establishments as $establishment)
             <x-location-card :establishment="$establishment" />
         @endforeach
