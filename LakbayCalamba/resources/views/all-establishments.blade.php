7e42b0f03ed06f341d031df119295aab7de5d2e3
trishamlpz <160499672+alliahf23@users.noreply.github.com>
2025-10-18 12:13:17 +0800

    fixing image

--- LakbayCalamba/resources/views/all-establishments.blade.php
+++ LakbayCalamba/resources/views/all-establishments.blade.php
@@ -81,7 +81,7 @@
     </div>
     
     <!-- Establishments Grid -->
-    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mb-8" id="establishmentsGrid"> 
+    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 mb-8 justify-items-center" id="establishmentsGrid"> 
         @foreach($establishments as $establishment)
             <x-location-card :establishment="$establishment" />
         @endforeach
